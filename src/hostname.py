3fa245a5e5cea31f11a5c4816bacbf4147f6b26a
James Legg <j.legg.17@ucl.ac.uk>
2021-11-08 17:18:34 +0000

    in hostname.py do something with the args file argument

--- src/hostname.py
+++ src/hostname.py
@@ -1,4 +1,4 @@
-#!python3
+#!/usr/bin/env python3
 
 # tested with module pyton/3.9.6
 
@@ -13,11 +13,18 @@ if __name__ == '__main__':
     print('command line arguments passed to this python script:')
     for command_line_arg in sys.argv[1:]:
         print(f'{command_line_arg = }')
-    shape = os.environ['SHAPE'] if name in os.environ.keys() else None
+    shape = os.environ['SHAPE'] if 'SHAPE' in os.environ.keys() else None
     if shape is not None:
         print('Ooo you passed this shape info with the original job submission request: {}'.format(shape))
         print("So the arguments after this program's name in gerun were passed to each instance of this program.")
     else:
         print("I would have told you about shapes but you did not pass 'S' or 'C' to the original job submission request!", file=sys.stderr)
+    a_pos = sys.argv.index('-A')
+    if a_pos > 1:
+        args_file = sys.argv[a_pos + 1]
+        with open(args_file, 'r') as f:
+            lines = f.readlines()
+        print('Here are some arguments I picked up from the specified arguments file:')
+        for line in lines: print('---> {}'.format(line))
     print('Leaving the hostname program, which has retrieved the following values:')
     print('*' * 80)
